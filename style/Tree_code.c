f24e7d6a698b2bc5dc1bbe9ca4e01af3e9cfb102
CodeCanvasCrafter <eduplaynayeem@gmail.com>
2024-05-16 23:40:51 +0600

    new Website design

---
+++ style/Tree_code.c
@@ -0,0 +1,23 @@
+  #include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Declare variables to store the two numbers
+    int num1, num2;
+
+    // Ask the user to enter the first number
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    // Ask the user to enter the second number
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    // Calculate the sum
+    int sum = num1 + num2;
+
+    // Display the sum
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+
+    return 0;
+}
